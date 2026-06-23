1fd54728f9ef2f058bbef71fa05089c9049e969b
Dipesh Singh Sengar <dipeshsengar2006@gmail.com>
2026-06-23 23:21:33 +0530

    for loop questions

--- python/list1.py
+++ python/list1.py
@@ -1,3 +1,8 @@
+'''''''''''''''''''''''''''''''''''''''''git commands '''
+# git add *
+# git commit -m "Explaint the commit here"
+# git push
+
 '''''''''''''''''''''''''''''''''List'''
 
 # numbers = [ 10, 5, 7, 2, 1]
@@ -40,10 +45,59 @@
 # print(numbers[-1])
 
 """""question 1"""
-numbers = [ 1, 2, 3, 4, 5]
-print(len(numbers))
-del numbers[-1]
-print(numbers)
-x = int(input("enter your replaced number: "))
-numbers[int(len(numbers)/2)] = x
-print(numbers)
+# numbers = [ 1, 2, 3, 4, 5]
+# print(len(numbers))
+# del numbers[-1]
+# print(numbers)
+# x = int(input("enter your replaced number: "))
+# numbers[int(len(numbers)/2)] = x
+# print(numbers)
+
+
+'''''''append '''
+
+# list = [5, 4, 3, 2, 1]
+# print(list)
+# print(f'Length of list: {len(list)}')
+# list.append(6)
+# print(list)
+# print(f'Length  of list:{len(list)}')
+'''   f' kjSnjshk '    '''# we an use any thing in line with f'' like list, tuple...etc
+
+'''''''''''''''''''''''insert '''
+# numbers = [111, 7, 2, 1]
+# print(len(numbers))
+# print(numbers)
+
+# numbers.append(4)
+
+# print(len(numbers))
+# print(numbers)
+
+# numbers.insert(0, 222)
+# print(len(numbers))
+# print(numbers)
+
+'''problem '''
+# list1 = [1, 2, 3]
+# for count in range(len(list1)):
+#   print(list1[count])
+
+'''problem '''
+# my_list=[]
+# for i in range(1,11, 1):
+#   my_list.append(i)
+# print(my_list)
+# '''''''''from while loop'''
+# count = 1
+# while count<=10:
+#   my_list.append(count)
+#   count += 1
+# print(my_list)
+
+'''problem '''
+# my_list=[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# for i in range(10):
+#   my_list[i] += 1
+  
+# print(my_list)
--- python/loops.py
+++ python/loops.py
@@ -97,7 +97,7 @@
 #     else:
 #         print("Leap year")
 
-'''for loop ''''''''when range is known '''
+'''for loop ''''''''(when range is known) '''
 '''''''''''''''''''''''''''''''''''''''''''''''''''' Break in for loop '''''''''''''''''''''''''''''''''
 # print("The break instructions: ");
 # for counter in range(1, 6):
@@ -105,6 +105,58 @@
 #         break
 #     print("Inside the loop. ", counter)
 # print("Output the loop. ")
+
+'''''''''''''''''''''''''''''''''''''''''extra for loop questions'''
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+#   print(x)
+
+#for x in "banana":
+#   print(x) 
+
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+#   if x == "banana":
+#     break
+#   print(x)
+
+
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
+
+#for x in range(6):
+#   print(x)
+
+#for x in range(2, 6):
+#   print(x)
+
+#for x in range(2, 30, 3):
+#   print(x)
+
+
+#for x in range(6):
+#   print(x)
+# else:
+#   print("Finally finished!")
+
+
+
+
+
+
+
+
+
+
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''' coninue in for loop '''''''''''''''''''''''''''''''''
 
