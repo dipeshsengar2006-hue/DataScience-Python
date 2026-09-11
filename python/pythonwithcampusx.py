bedd4ee34b4ab460b43075ff81c3baab26c8c43f
Dipesh Singh Sengar <dipeshsengar2006@gmail.com>
2026-09-12 02:37:12 +0530

    Python with Campux ( print functions, Data Types)

---
+++ python/pythonwithcampusx.py
@@ -0,0 +1,62 @@
+"""Print functions"""
+
+# print("hello") # hello
+ 
+# print(5) # 5
+
+# print("Dipesh", "Tilak", "Vinay") # Dipesh Tilak Vinay
+
+# print(True)  #True
+
+# print("India", 5, True)  # Indis 5 True
+
+# print("Dipesh", "Tilak", "Vinay", sep= "/") # Dipesh/Tilak/Vinay
+# print("Dipesh", "Tilak", "Vinay", sep= "-") # Dipesh-Tilak-Vinay
+
+# print("Hello")  
+# print("World")
+#     # Hello 
+#     # World
+# print("Hello", end=" ")
+# print("Wolrd")
+    # Hello world
+
+
+"""
+Data Types (DT)
+1. BASIC TYPES - integer, float, boolean, complex and string
+2. CONTAINER TYPES - list, tuples, sets and dictionary
+3. USER DEFINED TYPES - Class (we will see this in oops)
+"""
+## 1.BASIC TYPES
+'''Integer'''
+#print(4)
+#print(1e309)
+
+'''float'''
+#print(1.5e204)
+
+'''boolean'''
+#print(True)
+#print(False)
+
+'''Complex'''
+#print(4 + 5j)
+
+'''String'''
+#print('India')
+#print("India")
+#print("""India""")
+
+## 2.CONTAINER TYPES
+'''list'''
+print([1, 2, 3, 4, 5]) #[1, 2, 3, 4, 5]
+
+'''tuple'''
+print((1, 2, 3, 4, 5)) #(1, 2, 3, 4, 5)
+
+'''sets'''
+print({1, 2, 3, 4, 5}) #{1, 2, 3, 4, 5}
+
+'''Dictionary'''
+print({"Name": "Dipesh", "Age": 20, "Gender": "Male"}) #{'Name': 'Dipesh', 'Age': 20, 'Gender': 'Male'}
